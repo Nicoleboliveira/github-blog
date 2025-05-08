48deace9f66eeebf4004f8930ce74ad75a82f115
nicole <nicoleboliveira14@gmail.com>
2025-05-08 14:32:28 -0300

    feat: adicionar nova rota e estrutura de pasta para [Home]

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
 import { GlobalStyle } from "./styles/global";
 import { defaultTheme } from "./styles/themes/default";
+import { Home } from "./pages/Home";
 
 export function App() {
   return (
-    <ThemeProvider theme={defaultTheme}>
-      <GlobalStyle />
-      <h1>Hello word</h1>;
-    </ThemeProvider>
+    <BrowserRouter>
+      <ThemeProvider theme={defaultTheme}>
+        <GlobalStyle />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </ThemeProvider>
+    </BrowserRouter>
   );
 }
